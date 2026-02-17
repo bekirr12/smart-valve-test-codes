56a2d0e49283b1840b49b93456c3c820d4933777
Bekir Karatas <bekirsamikaratas@gmail.com>
2026-02-17 13:16:11 +0300

    added physical values function in adc test code

--- msp430fr60x7_ADC_TEST/msp430fr60x7_ADC_TEST.c
+++ msp430fr60x7_ADC_TEST/msp430fr60x7_ADC_TEST.c
@@ -1,6 +1,3 @@
-#include <msp430.h>
-#include <stdint.h>
-
 // All pins have to make low initally
 // pin definitions
 
@@ -17,7 +14,55 @@
 // PWH2 p2.6/ta4.1
 // PWL2 p3.5/tb0.4 (lehimle taşınacak)
 
+// Calculate currents, voltages, and power
+
+/*
+Panel voltage,
+Battery voltage,
+Panel current,
+battery current,
+panel power,
+battery power
+
+Voltage Constant
+resistor values: voltage divider --> 178k, 10k = rate 18.8.
+adc step: 3300mV/4095= 0.8058mV.
+constant value: 0.8058 x 18.8 = 15.15mV.
+Every adc step equals to 15.15mV.
+code constant: 15.15 x 4096 = 62054.
+
+Current Constant
+resistor values: 10k / 402 = (gain = 24.8750).
+shunt= 2mohm.
+Sense: 24.875x0.002 = 0.04975 V/A (49.75 mV/A).
+adc step: 3300mV/4095= 0.8058mV.
+current constant: (0.8058mV) / (0.04975 V/A) x 1000 = 16.197
+Every adc step equals to 16.2mA
+code constant: 16.2 x 4096 = 66343.
+
+*/
+
+
+
+#include <msp430.h>
+#include <stdint.h>
+
+#define V_MULTIPLIER    62054UL // voltage constant
+#define I_MULTIPLIER    66343UL // current constant
+
+#define SHIFT_AMOUNT    12  // X >> 12 means X / 4096.
+
 volatile uint16_t adc_results[4];
+
+// physical values
+typedef struct {
+    uint16_t Panel_Voltage_mV;
+    uint16_t Battery_Voltage_mV;
+    uint16_t Panel_Current_mA;
+    uint16_t Battery_Current_mA;
+    uint32_t Panel_Power_mW;
+    uint32_t Battery_Power_mW;
+} SystemData;
 
 void init_clock_external_8MHz(void){
     FRCTL0 = FRCTLPW | NWAITS_1;
@@ -39,6 +84,36 @@ void init_clock_external_8MHz(void){
     CSCTL3 = DIVA__1 | DIVS__1 | DIVM__1; // set divider 
 
     CSCTL0_H = 0;   // lock
+}
+
+void init_gpio_default_low(void) {
+    // GPIO Configuration
+    P1OUT = 0;
+    P1DIR = 0xFF;
+
+    P2OUT = 0;
+    P2DIR = 0xFF;
+
+    P3OUT = 0;
+    P3DIR = 0xFF;
+
+    P4OUT = 0;
+    P4DIR = 0xFF;
+
+    P5OUT = 0;
+    P5DIR = 0xFF;
+
+    P6OUT = 0;
+    P6DIR = 0xFF;
+
+    P7OUT = 0;
+    P7DIR = 0xFF;
+
+    P8OUT = 0;
+    P8DIR = 0xFF;
+
+    P9OUT = 0;
+    P9DIR = 0xFF;
 }
 
 void init_adc(void){
@@ -73,16 +148,54 @@ void read_adc(void){
     adc_results[2] = ADC12MEM2;
     adc_results[3] = ADC12MEM3;
 }
+/*
+// This function is for debugging purposes only. 
+// We don't need to use it in the firmware. 
+// We can perform our operations directly through the ADC values.
+void update_display_data(void){
+    // 1. voltage calculate: (ADC value * 62054) / 4096
+    uint32_t temp_calc;
+
+    temp_calc = (uint32_t)adc_results[0] * V_MULTIPLIER;
+    solar_data.Panel_Voltage_mV = (uint16_t)(temp_calc >> SHIFT_AMOUNT);
+
+    temp_calc = (uint32_t)adc_results[1] * V_MULTIPLIER;
+    solar_data.Battery_Voltage_mV = (uint16_t)(temp_calc >> SHIFT_AMOUNT);
+
+    // 2. current calculate: (ADC value * 66343) / 4096
+
+    // basic noise filter
+    if(adc_results[2] > 5) {
+        temp_calc = (uint32_t)adc_results[2] * I_MULTIPLIER;
+        solar_data.Panel_Current_mA = (uint16_t)(temp_calc >> SHIFT_AMOUNT);
+    } else {
+        solar_data.Panel_Current_mA = 0;
+    }
+
+    if(adc_results[3] > 5) {
+        temp_calc = (uint32_t)adc_results[3] * I_MULTIPLIER;
+        solar_data.Battery_Current_mA = (uint16_t)(temp_calc >> SHIFT_AMOUNT);
+    } else {
+        solar_data.Battery_Current_mA = 0;
+    }
+
+    // 3. power calculate: P(mW) = V(mV) * I(mA) / 1000
+    solar_data.Panel_Power_mW = ((uint32_t)solar_data.Panel_Voltage_mV * solar_data.Panel_Current_mA) / 1000;
+    solar_data.Battery_Power_mW = ((uint32_t)solar_data.Battery_Voltage_mV * solar_data.Battery_Current_mA) / 1000;
+
+}
+*/
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
+    WDTCTL = WDTPW | WDTHOLD; // Stop WDT
+    init_gpio_default_low(); // all pins low
     init_clock_external_8MHz();
     init_adc();
     PM5CTL0 &= ~LOCKLPM5;
     while(1) {
         read_adc();
-
+        // update_display_data();
         // delay
         __delay_cycles(100000); 
     }
